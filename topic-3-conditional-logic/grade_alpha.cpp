a8a7b5735cbf7117d62bb0bb64a3ff17d7ae6b27
Zidz MacBook Pro <zidz@Zidzs-MacBook-Pro-3.local>
2019-03-17 11:58:20 +0800

    third commit

---
+++ topic-3-conditional-logic/grade_alpha.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+  
+  int grade = 45;
+  
+  if (grade >= 50) {
+    
+    cout << "Pass\n";
+    
+  } else {
+    
+    cout << "Fail\n";
+    
+  }
+  
+  return 0;
+
+}
