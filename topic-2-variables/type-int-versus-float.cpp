6bf14e575d14ce03501b7da06beb65473b981b5d
Zidz MacBook Pro <zidz@Zidzs-MacBook-Pro-3.local>
2019-03-18 21:05:22 +0800

    int-vs-float

---
+++ topic-2-variables/type-int-versus-float.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 5;
+    float y = 5;
+
+    cout << x / 2 << endl; // output 2
+    cout << y / 2 << endl; // output 2.5
+
+    cout << x / 10 << endl; // output 0
+    cout << y / 10 << endl; // output 0.5
+}
