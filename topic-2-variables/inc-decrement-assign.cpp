40599efa5dd68d9ab9be131a47eb6c3918819ee1
Zidz MacBook Pro <zidz@Zidzs-MacBook-Pro-3.local>
2019-03-19 00:56:19 +0800

    increment-dec-assign

---
+++ topic-2-variables/inc-decrement-assign.cpp
@@ -0,0 +1,18 @@
+// learn increment-decrement concept
+// learn assignment method alternative
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 100;
+    x = x + 50; // the same as x += 50 ;
+
+    cout << x << endl;
+    cout << ++x << endl;
+    cout << x++ << endl;
+    cout << x << endl;
+}
+
+
