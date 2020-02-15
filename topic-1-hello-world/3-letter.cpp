4cc94d20dedbfb7093b3c7ef13727d5372917550
Yazid <zidz@i9-MacBook-Pro.local>
2020-02-15 18:58:42 +0800

    edit line

--- topic-1-hello-world/3-letter.cpp
+++ topic-1-hello-world/3-letter.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-  
+
   std::cout << "Dear Self,                      \n";
   std::cout << "                                \n";
-  std::cout << "I will buy the next PlayStation.\n";
+  std::cout << "I wanna buy both PS5 and Xbox Series X\n";
   std::cout << "                                \n";
   std::cout << "                         - Yazid\n";
-  
+
   return 0;
-  
+
 }
