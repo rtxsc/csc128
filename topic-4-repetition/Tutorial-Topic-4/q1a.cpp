0ba9dee7ee9de5cef5334a8f3c2fe0fa550bd856
zidz <zidios101@gmail.com>
2020-05-13 12:04:29 +0800

    upload

---
+++ topic-4-repetition/Tutorial-Topic-4/q1a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+ int sum=0, i=73;
+ // using while loop
+while(i<=415){
+  sum = sum + i;
+  i++;
+}
+cout << "Sum is:" << sum << endl;
+}
