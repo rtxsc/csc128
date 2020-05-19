3c7d2750e612c9c3507a1a2b8065af1146a0ae17
Robotronix <43515689+rtxsc@users.noreply.github.com>
2020-05-19 21:53:21 +0800

    Update q6.cpp

--- topic-4-repetition/Tutorial-Topic-4/q6.cpp
+++ topic-4-repetition/Tutorial-Topic-4/q6.cpp
@@ -7,13 +7,16 @@
 1
 */
 
-#include <iostream>
+#include "iostream"
+
 using namespace std;
+int main(){
 
-int main() {
-
-
-
-
-
+    int i,j;
+    for (i = 5; i > 0; i--) {
+      for(j = 0; j < i ; j++){
+        cout << i-j << " ";
+      }
+      cout << endl;
+    }
 }
